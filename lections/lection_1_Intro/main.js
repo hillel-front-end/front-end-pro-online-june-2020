e7e56b5da99e98a48be51044344c0c58e96927d5
Savelii.sychov <savelii.sychov@Admins-Mac-Pro.local>
2020-06-18 21:17:07 +0300

    L_1 upd

--- lections/lection_1_Intro/main.js
+++ lections/lection_1_Intro/main.js
@@ -1,4 +1,110 @@
 console.log('lection 1');
+ 
 
 
-x = 1;
+
+// variables
+
+x = 10;
+
+
+x = 60;
+y = 120;
+
+
+c = x + y;
+// c = 60 + y;
+// c = 60 + 120;
+// c = 180;
+
+// console.log( c );
+// console.log( 180 );\
+
+c = x - y;
+c = x * y;
+c = x / y;
+
+// a = 14;
+// b = 3
+
+// c = a % b; // mod
+// console.log(c);
+
+
+// data types
+
+// number
+// a = 10;
+// b = 20;
+// console.log( a * b );
+// console.log( a * b ); 
+
+
+// // string
+// example = 'Hello world';
+
+// console.log(example.toUpperCase());
+// console.log(example.toLowerCase());
+// console.log(example.substr(0, 4));
+
+
+
+// operations between string and number
+
+// int a = 10;
+// string str = 'hello world';
+// console.log( a + str );
+
+a = 10;
+str = '67';
+
+
+// type conversion
+console.log( a - str ); // str -> number -> 67
+console.log( a * str );
+console.log( a / str );
+
+// concatenation
+console.log( a + str ); 
+// console.log( 10 + '67' ); 
+// console.log( '10' + '67' ); 
+// console.log( '1067' ); 
+
+// ------------------------------------
+
+string = '524';
+// correctNumber = parseInt(string);
+// console.log( parseInt(string) );
+// console.log( parseFloat(string) );
+
+
+// unary operator
+
+//  a - (b - 3) -> a - b + 3x
+//  a + (-1)*(b - 3) -> a + (-b + 3) -> a - b + 3
+
+// k = -5;
+// console.log( +k, -k )
+
+
+// console.log( a + +str ); 
+// console.log( a + (+str) ); 
+// console.log( 10 + +'67' ); 
+// console.log( 10 + (+1)*'67' ); 
+// console.log( 10 + (+1)*67 ); 
+// console.log( 10 + 67 ); 
+// console.log( 77 ); 
+
+// -------------------------
+
+x = 15;
+y = 2;
+
+console.log( x > y );
+console.log( x >= y );
+console.log( x < y );
+console.log( x <= y );
+console.log( x != y );
+console.log( x == y );
+// console.log( x === y );
+
